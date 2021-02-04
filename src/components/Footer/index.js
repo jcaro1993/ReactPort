0830935e49862cd17bb834beb4e47dd195532dba
Javier Caro <jcaro1993@gmail.com>
2021-02-03 23:16:29 -0800

    added files

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css"
+
+function Footer() {
+  return (
+      <div className="footer">
+    <footer>
+      <hr />
+      <p><a href="https://www.linkedin.com/in/javier-caro-206b0217a" className="fa fa-linkedin-square"></a> <a href="https://github.com/jcaro1993" class="git fa fa-github-square"></a></p>
+    </footer>
+    </div>
+  );
+}
+
+export default Footer;
